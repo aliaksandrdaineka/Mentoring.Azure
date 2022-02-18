6611a91e9ab52c5f1df5aa3cc6d315d755dca7a3
Aliaksandr Daineka <daynekoas@gmail.com>
2022-02-18 17:34:26 +0100

    initial commit

---
+++ src/PublicApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using BlazorShared.Models;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Http;
+using Microsoft.eShopWeb.ApplicationCore.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.eShopWeb.PublicApi.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly TelemetryClient _telemetryClient;
+
+    public ExceptionMiddleware(RequestDelegate next, TelemetryClient telemetryClient)
+    {
+        _next = next;
+        _telemetryClient = telemetryClient;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        _telemetryClient.TrackException(exception);
+
+        if (exception is DuplicateException duplicationException)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.Conflict;
+            await context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = duplicationException.Message
+            }.ToString());
+        }
+    }
+}
---
+++ src/Web/Pages/Basket/Checkout.cshtml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.eShopWeb.ApplicationCore.Exceptions;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using Microsoft.eShopWeb.Infrastructure.Identity;
+using Microsoft.eShopWeb.Web.Interfaces;
+
+namespace Microsoft.eShopWeb.Web.Pages.Basket;
+
+[Authorize]
+public class CheckoutModel : PageModel
+{
+    private readonly IBasketService _basketService;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly IOrderService _orderService;
+    private string _username = null;
+    private readonly IBasketViewModelService _basketViewModelService;
+    private readonly IAppLogger<CheckoutModel> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public CheckoutModel(IBasketService basketService,
+        IBasketViewModelService basketViewModelService,
+        SignInManager<ApplicationUser> signInManager,
+        IOrderService orderService,
+        IAppLogger<CheckoutModel> logger,
+        IConfiguration configuration,
+        IHttpClientFactory httpClientFactory)
+    {
+        _basketService = basketService;
+        _signInManager = signInManager;
+        _orderService = orderService;
+        _basketViewModelService = basketViewModelService;
+        _logger = logger;
+        _configuration = configuration;
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public BasketViewModel BasketModel { get; set; } = new BasketViewModel();
+
+    public async Task OnGet()
+    {
+        await SetBasketModelAsync();
+    }
+
+    public async Task<IActionResult> OnPost(IEnumerable<BasketItemViewModel> items)
+    {
+        try
+        {
+            await SetBasketModelAsync();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var updateModel = items.ToDictionary(b => b.Id.ToString(), b => b.Quantity);
+            await _basketService.SetQuantities(BasketModel.Id, updateModel);
+
+            var address = new Address("123 Main St.", "Kent", "OH", "United States", "44240");
+            var orderId = await _orderService.CreateOrderAsync(BasketModel.Id, address);
+
+            await using var serviceBusClient = new ServiceBusClient(_configuration.GetValue<string>("ServiceBus:ConnectionString"));
+            await using ServiceBusSender sender = serviceBusClient.CreateSender(_configuration.GetValue<string>("ServiceBus:QueueName"));
+
+            var json = JsonSerializer.Serialize(new { orderId = orderId, items = BasketModel.Items.Select(x => new { x.CatalogItemId, x.Quantity }).ToArray() });
+            var message = new ServiceBusMessage(json);
+
+            await sender.SendMessageAsync(message);
+
+            var client = _httpClientFactory.CreateClient();
+
+            await client.PostAsJsonAsync(
+                _configuration.GetValue<string>("Functions:DeliveryOrderProcessor:Url"),
+                new { id = orderId, address = address, items = BasketModel.Items.Select(x => new { x.CatalogItemId, x.Quantity }).ToArray(), total = BasketModel.Items.Sum(x => x.UnitPrice * x.Quantity) }
+            );
+
+            await _basketService.DeleteBasketAsync(BasketModel.Id);
+        }
+        catch (EmptyBasketOnCheckoutException emptyBasketOnCheckoutException)
+        {
+            //Redirect to Empty Basket page
+            _logger.LogWarning(emptyBasketOnCheckoutException.Message);
+            return RedirectToPage("/Basket/Index");
+        }
+
+        return RedirectToPage("Success");
+    }
+
+    private async Task SetBasketModelAsync()
+    {
+        if (_signInManager.IsSignedIn(HttpContext.User))
+        {
+            BasketModel = await _basketViewModelService.GetOrCreateBasketForUser(User.Identity.Name);
+        }
+        else
+        {
+            GetOrSetBasketCookieAndUserName();
+            BasketModel = await _basketViewModelService.GetOrCreateBasketForUser(_username);
+        }
+    }
+
+    private void GetOrSetBasketCookieAndUserName()
+    {
+        if (Request.Cookies.ContainsKey(Constants.BASKET_COOKIENAME))
+        {
+            _username = Request.Cookies[Constants.BASKET_COOKIENAME];
+        }
+        if (_username != null) return;
+
+        _username = Guid.NewGuid().ToString();
+        var cookieOptions = new CookieOptions();
+        cookieOptions.Expires = DateTime.Today.AddYears(10);
+        Response.Cookies.Append(Constants.BASKET_COOKIENAME, _username, cookieOptions);
+    }
+}
